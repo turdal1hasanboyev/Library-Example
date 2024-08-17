23cc97e62dbb9d8ecaf0f8cfe1a84b0696d8348a
turdal1hasanboyev <turdalihasanboyev70.com>
2024-08-17 08:39:28 +0500

    Updating project and Finish Project!

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,9 @@ from library.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id', 'subtitle', 'title', 'author', 'isbn')
+        fields = ('id', 'subtitle', 'title', 'author', 'isbn', "created_at")
+
+        extra_kwargs = {
+            'created_at': {'read_only': True},
+            'id': {'read_only': True},
+        }
--- library/models.py
+++ library/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=225, null=True, blank=True)
+    author = models.CharField(max_length=225, null=True, blank=True)
     subtitle = models.CharField(max_length=225, null=True, blank=True)
     isbn = models.CharField(max_length=225, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
     
